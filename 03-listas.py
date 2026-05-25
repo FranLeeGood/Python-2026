e600598e9d731296dc1e9e39d8d5cddd207d28ec
francisco hernandez <franciscoojedahernandez123@gmail.com>
2026-05-25 01:46:02 -0400

    Subiendo listas

---
+++ 03-listas.py
@@ -0,0 +1,16 @@
+#listas 
+
+#primera forma de declaracion de listas 
+lista1 = ["francisco", 32,True, 32, "francisco", "francsico"]
+
+#segunda forma de declaracion de listas 
+n = list([1,2,3,4,5])
+
+print(type(n))
+print(type(lista1))
+
+#metodos para las listas 
+
+#01-count ()
+
+print(lista1.count("francisco"))
