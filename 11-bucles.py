e46a350157732b5634da631acc4abb9ada9d0aa7
francisco hernandez <franciscoojedahernandez123@gmail.com>
2026-07-15 01:47:40 -0400

    Subiendo Bucles

--- 11-bucles.py
+++ 11-bucles.py
@@ -43,3 +43,48 @@ while num <=300:
         print("Mi condicion es igual a 250")
 print(Fore.GREEN + 'Tercer Bucle Terminado!')
 
+# Utilizar break
+while num <= 400:
+    print(num)
+    num +=2
+    if num == 350:
+        print(Fore.MAGENTA + 'Se detiene el bucle')
+        break
+print(num)
+print(Fore.MAGENTA + "Cuarto bucle Terminado!")
+
+#Utilizar el continue
+
+num = 0
+while num<= 50:
+    print(num)
+    num +=1
+    if num == 40:
+        continue
+    print(num)
+
+#bucle infinito + break
+  #  while True:
+  #   parametro = input(">>> ingrese la palabra secreta")
+   #  if parametro =="exit":
+  #      break
+   #  else:
+       #  print(parametro)
+
+
+     #bucle for 
+     #for n°1
+print(Fore.GREEN + "====BUCLES FOR ======")
+for i in (1,2,3,4,5,6,7,8,9,10):
+        print(i)
+
+#iterando una lista
+listita = [1,2,3,4,5,6,7,8,9,10]
+
+for i in listita:
+    print(i)
+
+
+    # iterando una tercera forma (3° For)
+for i in range(1,101):
+    print(i)
